58af9248c580ec03ece1d9059b6ceb904737111e
Reckdev27 <reckdev27@gmail.com>
2026-02-01 16:56:05 +0800

    update

--- Assets/Scripts/PokerGame/Client/General/TipController.cs
+++ Assets/Scripts/PokerGame/Client/General/TipController.cs
@@ -3,6 +3,8 @@
 ////////////////////
 
 
+using Com.poker.Core;
+using Google.Protobuf;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -14,7 +16,10 @@ namespace WCC.Poker.Client
     {
         [SerializeField] TMP_InputField _tipIF;
         [SerializeField] Button _confirmButton;
-     
+
+        long _maxStack;
+
+        private void Awake() => PokerNetConnect.OnMessageEvent += OnMessage;
 
         private void Start()
         {
@@ -22,14 +27,43 @@ namespace WCC.Poker.Client
             _tipIF.onValueChanged.AddListener(OnChangeListener);
         }
 
+        void OnTableSnapshot(TableSnapshot m)
+        {
+            foreach (var p in m.Players)
+            {
+                if (p.PlayerId == PokerNetConnect.OwnerPlayerID && _maxStack != p.Stack)
+                    _maxStack = p.Stack;
+            }
+        }
+
+        void OnMessage(MsgType type, IMessage msg)
+        {
+            if (type == MsgType.TableSnapshot)
+            {
+                OnTableSnapshot((TableSnapshot)msg);
+            }
+        }
+
         void OnClickSendTipButton()
         {
-
+            GameServerClient.SendTipStatic(GameServerClient.Instance.TableId, int.Parse(_tipIF.text));
         }
 
         void OnChangeListener(string change)
         {
             _confirmButton.interactable = change != string.Empty;
+        }
+
+        void OnEnable() => GameServerClient.TipResponseReceivedStatic += OnTipResponse;
+
+        void OnDisable() => GameServerClient.TipResponseReceivedStatic -= OnTipResponse;
+
+        void OnTipResponse(TipResponse resp)
+        {
+            if (resp.Success)
+                Debug.Log($"Tip ok amount={resp.Amount} newStack={resp.Stack}"); //--eto na yong udpated balance mo after ka mag bigay ng tip. Make an animation na merong chips na papunta sa Girl na nasa table
+            else
+                Debug.Log($"Tip failed reason={resp.Message}");
         }
     }
 }
